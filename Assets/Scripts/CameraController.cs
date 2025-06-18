4ba111abb25e06f8383e87e92b83a6d00cb074fc
Brian L <brianleip@gmail.com>
2025-06-18 16:15:38 -0700

    Switching from image zoomer to camera controller script.  First pass

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class CameraController : MonoBehaviour, IScrollHandler {
+
+	[SerializeField] private Camera cam;
+	[SerializeField] private float zoomSpeed = 0.01f;
+	[SerializeField] private float smoothingSpeed = 4f;
+	[SerializeField] private float maxZoom = 5f;
+	private Vector3 initialScale, targetScale;
+	private Vector3 dragStart, dragVec;
+
+
+	private void Update() {
+
+		if (Mouse.current.leftButton.isPressed) {
+			Vector3 mouseWorldPos = cam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+			if (Mouse.current.leftButton.wasPressedThisFrame) {
+				Debug.Log("Pan start");
+				dragStart = mouseWorldPos;
+			}
+			dragVec = dragStart - mouseWorldPos;
+			Debug.Log($"Pan Vector currently is {dragVec.ToString()}");
+		}
+
+		if (Mouse.current.leftButton.wasReleasedThisFrame)
+			PanCamera();
+	}
+
+	private void PanCamera() {
+		cam.transform.position += dragVec;
+		Debug.Log("Pan end");
+	}
+
+	private void ZoomCamera() {
+		
+	}
+
+	public void OnScroll(PointerEventData eventData) {
+		Vector3 delta = Vector3.one * (eventData.scrollDelta.y * zoomSpeed);
+		targetScale = transform.localScale + delta;
+
+		targetScale = ClampTargetScale(targetScale);
+	}
+
+	private Vector3 ClampTargetScale(Vector3 scale) {
+		scale = Vector3.Max(initialScale, scale);
+		scale = Vector3.Min(initialScale * maxZoom, scale);
+		return scale;
+	}
+}
--- Assets/Scripts/UIZoomImage.cs
+++ Assets/Scripts/UIZoomImage.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+// TODO: Keep image centered on mouse/touch position.  Make sure image stays within bounds of screen.
+// TODO: Touch input zooming
+
 public class UIZoomImage : MonoBehaviour, IScrollHandler {
 
 	[SerializeField] private float zoomSpeed = 0.01f;
@@ -15,7 +18,7 @@ public class UIZoomImage : MonoBehaviour, IScrollHandler {
 
 	private void Update() {
 		bool zoomingIn = targetScale.x > transform.localScale.x;
-		float smoothSpeedInOut = zoomingIn ? smoothingSpeed / 2 : smoothingSpeed;	// Zoom in is faster than zoom out, so adjust speed
+		float smoothSpeedInOut = zoomingIn ? smoothingSpeed / 2 : smoothingSpeed; // Zoom in is faster than zoom out, so adjust speed
 		transform.localScale = Vector3.Lerp(transform.localScale, targetScale, Time.deltaTime * smoothSpeedInOut);
 	}
 
