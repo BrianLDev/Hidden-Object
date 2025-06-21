b8701af4422cb24eccc2d4e8bad2725092f847a4
Brian L <brianleip@gmail.com>
2025-06-20 23:27:59 -0700

    Fixed jitter when zoomed out too far, added auto zoom in

--- Assets/Scripts/ImageManager.cs
+++ Assets/Scripts/ImageManager.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
 using EcxUtilities;
+using Lean.Touch;
 
 public class ImageManager : Singleton<ImageManager> {
-	[SerializeField] Camera cam;
-	[SerializeField] SpriteRenderer spriteRenderer;
+	[SerializeField] private Camera cam;
+	[SerializeField] private SpriteRenderer spriteRenderer; // for the hidden object image
+	[SerializeField] public LeanPinchCamera leanPinchCamera;
+	[SerializeField] private float zoomSpeed = 3f;
+	[SerializeField] private float zoomInFraction = 0.05f;
+	[HideInInspector] public float targetZoom;
+	private const float epsilon = 0.0001f;	// TODO: Move this to a global constants class at some point
+
+
+	private void Start() {
+		if (cam == null)
+			cam = Camera.main;
+		if (spriteRenderer == null)
+			Debug.LogError("Error: Image manager needs a reference to the Hidden Object's sprite renderer");
+		if (leanPinchCamera == null)
+			leanPinchCamera = FindAnyObjectByType<LeanPinchCamera>();
+		targetZoom = leanPinchCamera.Zoom;
+	}
+
+	private void Update() {
+		if (Mathf.Abs(leanPinchCamera.Zoom - targetZoom) > epsilon)
+			leanPinchCamera.Zoom = Mathf.Lerp(leanPinchCamera.Zoom, targetZoom, Time.deltaTime * zoomSpeed);
+	}
 
 	public Vector3 ClampBounds(Vector3 targetPosition) {
 		Vector3 camSize = new Vector3(cam.orthographicSize * cam.aspect, cam.orthographicSize, 0);
 		Vector3 minPos = GetImageMinPos() + new Vector3(camSize.x, camSize.y, 0);
 		Vector3 maxPos = GetImageMaxPos() - new Vector3(camSize.x, camSize.y, 0);
+		if (minPos.x > maxPos.x || minPos.y > maxPos.y) {
+			// Debug.Log($"Min image pos {minPos} > max image pos {maxPos}.  Zooming in");
+			ZoomIn(zoomInFraction);
+			return targetPosition;
+		}
 		Vector3 newTargetPos;
-		newTargetPos.x  = Mathf.Clamp(targetPosition.x, minPos.x, maxPos.x);
+		newTargetPos.x = Mathf.Clamp(targetPosition.x, minPos.x, maxPos.x);
 		newTargetPos.y = Mathf.Clamp(targetPosition.y, minPos.y, maxPos.y);
 		newTargetPos.z = targetPosition.z;
 		// Debug.Log($"targetPos {targetPosition}, ImageMin {GetImageMinPos()}, ImageMax {GetImageMaxPos()}, minPos {minPos}, maxPos {maxPos}, newTargetPos {newTargetPos}");
@@ -22,4 +49,9 @@ public class ImageManager : Singleton<ImageManager> {
 
 	public Vector3 GetImageMaxPos() =>
 		spriteRenderer.transform.position + spriteRenderer.bounds.size / 2;
+
+	// TODO: Consider merging/centralizing 3 zoom methods (1. this, 2. LeanPinchCamera, 3. ScrollZoomCamera)
+	public void ZoomIn(float fraction) =>
+		targetZoom = leanPinchCamera.Zoom * (1 - fraction);
+
 }
--- Assets/Scripts/ScrollZoomCamera.cs
+++ Assets/Scripts/ScrollZoomCamera.cs
@@ -1,32 +1,30 @@
 using UnityEngine;
-using Lean.Touch;
 using UnityEngine.InputSystem;
+using Lean.Touch;
+
+// TODO: Zoom in on mouse position
 
 public class ScrollZoomCamera : MonoBehaviour {
 
-	[SerializeField] LeanPinchCamera leanPinchCamera;
-	[SerializeField] private float zoomSpeed = 5f;	// actual values are around .005, so multiply/divide by 1000 when using
-	[SerializeField] private float smoothingSpeed = 2f;
+	[SerializeField] private float scrollZoomFactor = 5f; // actual values are around .005, so multiply/divide by 1000 when using
 	private InputAction scrollAction;
 	private Vector2 scrollDelta;
-	private float targetZoom;
+	private LeanPinchCamera leanPinchCamera;
 
 
 	private void Start() {
-		if (leanPinchCamera == null)
-			leanPinchCamera = FindAnyObjectByType<LeanPinchCamera>();
 		scrollAction = InputSystem.actions.FindAction("ScrollWheel");
-		targetZoom = leanPinchCamera.Zoom;
+		leanPinchCamera = ImageManager.Instance.leanPinchCamera;
 	}
 
 	private void Update() {
 		scrollDelta = -scrollAction.ReadValue<Vector2>();
+
 		if (scrollDelta.y != 0) {
-			scrollDelta.y = scrollDelta.y > 0 ? scrollDelta.y / 2 : scrollDelta.y;	// slow zoom in since its faster than zoom out
-			targetZoom += scrollDelta.y * (1 + zoomSpeed / 1000);
-			targetZoom = Mathf.Clamp(targetZoom, leanPinchCamera.ClampMin, leanPinchCamera.ClampMax);
+			scrollDelta.y = scrollDelta.y > 0 ? scrollDelta.y / 2 : scrollDelta.y;  // slow zoom in since its faster than zoom out
+			ImageManager.Instance.targetZoom += scrollDelta.y * (1 + scrollZoomFactor / 1000);
+			ImageManager.Instance.targetZoom = Mathf.Clamp(ImageManager.Instance.targetZoom, leanPinchCamera.ClampMin, leanPinchCamera.ClampMax);
 		}
-		leanPinchCamera.Zoom = Mathf.Lerp(leanPinchCamera.Zoom, targetZoom, Time.deltaTime * smoothingSpeed);
 	}
 
 }
